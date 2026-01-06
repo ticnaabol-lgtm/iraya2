4f4749ad57364ef051920333db449aa2e6cb8aee
ticnaabol-lgtm <tic.naabol@gmail.com>
2026-01-06 15:38:41 -0400

    Update footer.blade.php
    actualización del año 2026

--- laravel.movimientos/resources/views/layouts/partials/footer.blade.php
+++ laravel.movimientos/resources/views/layouts/partials/footer.blade.php
@@ -1,4 +1,4 @@
 <div class="flex-grow-1"></div>
 <div class="bg-card px-lg py-md d-flex justify-content-between align-items-center flex-wrap shadow-6dp">
-    <p class="text-muted m-0">Copyright &copy; 2025 | Navegación Aérea y Aeropuertos Bolivianos NAABOL</p>
+    <p class="text-muted m-0">Copyright &copy; 2026 | Navegación Aérea y Aeropuertos Bolivianos NAABOL</p>
 </div>
